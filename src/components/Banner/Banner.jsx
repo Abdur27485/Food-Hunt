9534a1e34863ccfc48f227c36bb8b690c345682b
Abdur Rahman <abdurrahman27485@gmail.com>
2023-05-04 12:47:29 +0600

    fixed hero-img issues

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import heroImg from './../../../public/assets/hero-img.jpg'
 
 const Banner = () => {
     return (
         <div className='hero-section'>
             <div className="hero min-h-screen bg-base-200 bg-opacity-10 mb-20">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img src="/public/assets/homeHero/hero-img.jpg" className="max-w-sm lg:max-w-lg lg:ms-8 rounded-3xl shadow" />
+                    <img src={heroImg} className=" mt-10 sm:mt-0 lg:max-w-lg lg:ms-8 rounded-3xl shadow" />
                     <div>
                         <h1 className="text-5xl lg:text-7xl font-bold uppercase">Life is short, make it <span className='text-orange-500'>juicy</span> & <span className='text-yellow-600'>tender</span>.</h1>
                         <p className="py-6 text-slate-950 font-bold">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
