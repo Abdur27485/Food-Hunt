a072d452e2d11b7bb407d09af6eb1d617d2d6477
Abdur Rahman <abdurrahman27485@gmail.com>
2023-05-04 23:16:47 +0600

    readme file added

---
+++ README.md
@@ -0,0 +1,10 @@
+# Recipe Hunt
+### Features:
+1. Find best chefs of Indian cuisine.
+2. Detailed cooking method.
+3. Interactive ui for your convenience.
+4. Authentic Information.
+5. All the recipes are tasty as well as healthy too.
+
+--------------------------------------------------------
+#### Live Site Link: [Recipe Hunt](http://devsonket.com)
--- src/components/BlogCards/BlogCards.jsx
+++ src/components/BlogCards/BlogCards.jsx
@@ -2,53 +2,55 @@ import React from 'react';
 
 const BlogCards = () => {
     return (
-        <div className='flex flex-wrap gap-10 justify-center my-20 mx-5'>
-            <div className="card w-96 bg-slate-200 shadow-2xl">
-                <div className="card-body">
-                    <h2 className="card-title">Tell us the differences between uncontrolled and controlled components.</h2>
-                    <p>Those components which state and beheviour are controlled by the parent element is called a Controlled Component. This components rely on the parent component to updare their state and behaviour. The components which controls their states by their own is called a uncontrolled component. In controlled component the data flows from parent to that component and in uncontrolled component the data flows within the component. However, controlled components are considered a best practice and uncontrolled components are considered an alternate approach.</p>
+        <div>
+            <div className='flex flex-wrap gap-10 justify-center my-20 mx-5'>
+                <div className="card w-96 bg-slate-200 shadow-2xl">
+                    <div className="card-body">
+                        <h2 className="card-title">Tell us the differences between uncontrolled and controlled components.</h2>
+                        <p>Those components which state and beheviour are controlled by the parent element is called a Controlled Component. This components rely on the parent component to updare their state and behaviour. The components which controls their states by their own is called a uncontrolled component. In controlled component the data flows from parent to that component and in uncontrolled component the data flows within the component. However, controlled components are considered a best practice and uncontrolled components are considered an alternate approach.</p>
+                    </div>
                 </div>
-            </div>
 
-            <div className="card w-96 bg-slate-200 shadow-2xl">
-                <div className="card-body">
-                    <h2 className="card-title">How to validate React props using PropTypes.</h2>
-                    <p>PropTypes utility exports a numerous range of validators for configuring type definitions. Here are basic and requied prop types:
-                        <ul className=' list-disc'>
-                            <li>PropTypes.func: The prop should be a function
-                            </li>
-                            <li>PropTypes.object: The prop should be an object
-                            </li>
-                            <li>PropTypes.symbol: The prop should be a symbol</li>
-                            <li>PropTypes.array: The prop should be an array
-                            </li>
-                            <li>PropTypes.string: The prop should be a string
-                            </li>
-                            <li>PropTypes.number: The prop should be a number
-                            </li>
-                            <li>PropTypes.bool: The prop should be a Boolean
-                            </li>
-                            <li>PropTypes.any: The prop can be of any data type
-                            </li>
-                        </ul>
-                    </p>
+                <div className="card w-96 bg-slate-200 shadow-2xl">
+                    <div className="card-body">
+                        <h2 className="card-title">How to validate React props using PropTypes.</h2>
+                        <p>PropTypes utility exports a numerous range of validators for configuring type definitions. Here are basic and requied prop types:
+                            <ul className=' list-disc'>
+                                <li>PropTypes.func: The prop should be a function
+                                </li>
+                                <li>PropTypes.object: The prop should be an object
+                                </li>
+                                <li>PropTypes.symbol: The prop should be a symbol</li>
+                                <li>PropTypes.array: The prop should be an array
+                                </li>
+                                <li>PropTypes.string: The prop should be a string
+                                </li>
+                                <li>PropTypes.number: The prop should be a number
+                                </li>
+                                <li>PropTypes.bool: The prop should be a Boolean
+                                </li>
+                                <li>PropTypes.any: The prop can be of any data type
+                                </li>
+                            </ul>
+                        </p>
+                    </div>
                 </div>
-            </div>
 
-            <div className="card w-96 bg-slate-200 shadow-2xl">
-                <div className="card-body">
-                    <h2 className="card-title">Tell us the difference between nodejs and express js.</h2>
-                    <p>NodeJs is a package which provides the JavaScript run-time enviroment, On the other hand Express.js is framework that is defended on the NodeJs and helps to handle responses and requests. NodeJs is used to build server, input-output, event-driven apps etc. Express.js is used to build web-application using the approaches and principles of NodeJs. The most noticeable difference is NodeJs built on Google's V8 engine, on the other side ExpressJs is built on NodeJs. To build something NodeJs takes more time than ExpressJs. ExpressJs also provides routing and controllers</p>
+                <div className="card w-96 bg-slate-200 shadow-2xl">
+                    <div className="card-body">
+                        <h2 className="card-title">Tell us the difference between nodejs and express js.</h2>
+                        <p>NodeJs is a package which provides the JavaScript run-time enviroment, On the other hand Express.js is framework that is defended on the NodeJs and helps to handle responses and requests. NodeJs is used to build server, input-output, event-driven apps etc. Express.js is used to build web-application using the approaches and principles of NodeJs. The most noticeable difference is NodeJs built on Google's V8 engine, on the other side ExpressJs is built on NodeJs. To build something NodeJs takes more time than ExpressJs. ExpressJs also provides routing and controllers</p>
+                    </div>
                 </div>
-            </div>
 
-            <div className="card w-96 bg-slate-200 shadow-2xl">
-                <div className="card-body">
-                    <h2 className="card-title">What is a custom hook, and why will you create a custom hook?</h2>
-                    <p>Those which hooks are created by developers are called custom hooks. We use custom hook to reduce the time to write the code and make our code cleaner. As custom hook doesn't needed to be rendered again it enhances the rendering speed. Custom hooks have the ability to be shared between components. Custom hooks improves the readability of the code. React Custoom Hook is a powerful thing which saves time and makes developers reliable. </p>
+                <div className="card w-96 bg-slate-200 shadow-2xl">
+                    <div className="card-body">
+                        <h2 className="card-title">What is a custom hook, and why will you create a custom hook?</h2>
+                        <p>Those which hooks are created by developers are called custom hooks. We use custom hook to reduce the time to write the code and make our code cleaner. As custom hook doesn't needed to be rendered again it enhances the rendering speed. Custom hooks have the ability to be shared between components. Custom hooks improves the readability of the code. React Custoom Hook is a powerful thing which saves time and makes developers reliable. </p>
+                    </div>
                 </div>
-            </div>
 
+            </div>
         </div>
     );
 };
